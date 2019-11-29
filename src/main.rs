31769064652d2e197a9908ba9d9f0099dee00640
konstantinos <klampropoulos@inf.uth.gr>
2019-11-29 19:34:02 +0200

    array element search

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-#![allow(dead_code, unused_variables)]
-fn elem_search(arr: &mut Vec<u32>) -> Option<u32> {
-    let index : i32 = -1;
-    Some(3)
-    
+
+// Returns the index of the position in the array
+fn elem_search(arr: &Vec<i32>, elem: i32) -> Option<usize> 
+{
+    for (i, pair) in arr.iter().enumerate() 
+    {
+        if *pair == elem 
+        {
+            return Some(i)
+        } 
+    }
+    None
 }
 
-
-fn bubblesort(arr: &mut Vec<u32>)
+// performs the classic Bubblesort algorithm
+fn bubblesort(arr: &mut Vec<i32>) ->  &Vec<i32>
 {
+
     let mut _temp = 0;
     let  arr_len = arr.len();
-
     for i in 0..arr_len
     {
-        //println!("i = [{:?}], ", i);
         let mut j = arr_len - 1;
-        while j > i { 
-
-            //println!("j = {}", j);
-            if arr[j] < arr[j-1]{
-
+        while j > i 
+        { 
+            if arr[j] < arr[j-1]
+            {
                 _temp = arr[j-1];
                 arr[j-1] = arr[j];
                 arr[j] = _temp;
@@ -28,14 +33,42 @@ fn bubblesort(arr: &mut Vec<u32>)
         }
     }
     println!("\nBubblesort => Array contents => {:?}", arr);
+    arr
 }
 
 
 fn main() 
 {
     let mut v  = vec![50, 30, 60, 90, 120, 5, 67, 63];
+    
     println!("\nInitial Array contents => {:?}",v);
+    
     bubblesort(&mut v);
+    
     println!("\nMain: => Array contents => {:?}", v);
 
+    match  elem_search(&v, 67) {
+        Some(num) => println!("Element [{}] exists!!", num),
+        None  => println!("Element does't exist in the array!"),
+    }
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn bubble_search_works() 
+    {
+        let test_vec : Vec<i32> =  vec![1, 2, 3, 4, 5];
+        assert_eq!(bubblesort(&mut vec![5, 4, 3, 2, 1]), &test_vec);
+        assert_ne!(bubblesort(&mut vec![5, 3, 3, 2, 1]), &test_vec);
+    }
+    #[test]
+    fn elem_search_works() 
+    {
+        let test_vec : Vec<i32> =  vec![1, 2, 3, 4, 5];
+        assert_eq!(elem_search(&test_vec, 3), Some(2));
+    }
 }
