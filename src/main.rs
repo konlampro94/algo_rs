bdda4cf5b05f39a68b52c3960ffd63dd86de1c6e
konstantinos <klampropoulos@inf.uth.gr>
2019-11-29 18:42:28 +0200

    suppress unused warnings

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-
+#![allow(dead_code, unused_variables)]
 fn elem_search(arr: &mut Vec<u32>) -> Option<u32> {
     let index : i32 = -1;
+    Some(3)
     
 }
 
 
 fn bubblesort(arr: &mut Vec<u32>)
 {
-    let mut temp = 0;
-    //println!("temp => [{:?}]", temp);
+    let mut _temp = 0;
     let  arr_len = arr.len();
-    println!("\nLength of vec => [{:?}]",arr_len);
+
     for i in 0..arr_len
     {
-        println!("i = [{:?}], ", i);
+        //println!("i = [{:?}], ", i);
         let mut j = arr_len - 1;
         while j > i { 
 
-            println!("j = {}", j);
+            //println!("j = {}", j);
             if arr[j] < arr[j-1]{
 
-                temp = arr[j-1];
+                _temp = arr[j-1];
                 arr[j-1] = arr[j];
-                arr[j] = temp;
+                arr[j] = _temp;
             }
         j = j - 1;
         }
@@ -34,8 +34,8 @@ fn bubblesort(arr: &mut Vec<u32>)
 fn main() 
 {
     let mut v  = vec![50, 30, 60, 90, 120, 5, 67, 63];
-    println!("\nInitial array => {:?}",v);
+    println!("\nInitial Array contents => {:?}",v);
     bubblesort(&mut v);
-    println!("\nMain: => array => {:?}", v);
+    println!("\nMain: => Array contents => {:?}", v);
 
 }
